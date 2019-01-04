72715917d330bc7a0e2acb3dc6b21a35968252c4
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-04 02:44:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ function add(a, b) {
 function subtract(a, b) {
   return a - b
 }
+function multiply(a, b) {
+  return a - b
+}
+function divide(a, b) {
+  return a - b
+}
+function inc(n) {
+  return a - b
+}
+function dec(n)) {
+  return a - b
+}
+function makeInt(n)) {
+  return a - b
+}
+function makeInt(n)) {
+  return a - b
+}
+function makeInt(n)) {
+  return a - b
+}
+function preserveDecimal(n) {
+  return a - b
+}
+function preserveDecimal(n) {
+  return a - b
+}
